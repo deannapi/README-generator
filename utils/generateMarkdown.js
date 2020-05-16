9513ec9302b7fe95c56bb8629893469da60b5a17
DeAnna Martinez <deannapi.mart@gmail.com>
2020-05-16 10:07:30 -0500

    changed folders and json

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,10 +2,34 @@
 function generateMarkdown(data) {
   return `# ${data.title}
     
-  ## About
+  ## Badges
+    ${data.badges}
+
+  ## Description
     * ${data.about}
 
-  ## Licenses
+  ## Table of Contents
+    * [Description](#description)
+    * [Demo](#demo)
+    * [Instructions](#instructions)
+    * [Usage](#usage)
+    * [Test](#test)
+    * [Credits](#credits)
+    * [License](#license)
+    
+  ## Installation
+    ${data.instructions}
+
+  ## Usage
+    ${data.usage}
+
+  ## Test
+    ${data.tests}
+
+  ## Credits
+    ${data.credits}
+
+  ## License
     * ${data.license}
 `;
 }
