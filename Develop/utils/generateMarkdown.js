6b706821dc4b125c2bb59f9fb126294af650b48c
DeAnna Martinez <deannapi.mart@gmail.com>
2020-05-15 20:53:29 -0500

    added readme questions and functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    
+  ## About
+    * ${data.about}
 
+  ## Licenses
+    * ${data.license}
 `;
 }
 
